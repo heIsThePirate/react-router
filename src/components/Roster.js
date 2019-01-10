e5a48b949b1ff2214f767bd334a1d3c03b5bbf45
Mohit Singh <mohitsingh1997@gmail.com>
2019-01-10 20:18:50 +0530

    Added Routing functionality

---
+++ src/components/Roster.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Roster extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Roster Page</h2>
+			</div>
+			);
+	}
+}
---
+++ src/components/Schedule.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Schedule extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Schedule Page</h2>
+			</div>
+			);
+	}
+}
